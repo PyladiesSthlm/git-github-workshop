92dc30239ec7e57be13f846bceccfda4e9f6d19b
sfro <sofia@soundtrackyourbrand.com>
2013-11-12 21:36:14 +0100

    Now with bugs.

--- demo_main.py
+++ demo_main.py
@@ -1,8 +1,20 @@
 from functions import Functions
 
 def main():
-	f = Functions("Hello world!")
-	f.printLetters()
+	
+	original_string = "Hello world!"
+
+	# Create an object f of type Functions. Internal f.__string is set to original_string at initialization
+	f = Functions(original_string)
+
+	
+	# If the internal variable f.__string has been changed, we check if it was changed using the setter f.setString()
+	if f.getString() != original_string and f.setterUsed() == False:
+		print "ERROR! Something is wrong here! Did you manipulate private variables in the Functions class directly??"
+		# Hint: We didn't, there is a bug in the Functions class	
+	else:
+		f.printString() # Print out f.__string	
+
 
 if __name__ == "__main__":
     main()
--- functions.py
+++ functions.py
@@ -1,17 +1,23 @@
 class Functions:
 
+	# Initializer that sets two 'private' internal variables
 	def __init__(self, string):
-		self._string = string
+		self.__string = string # Internal variable self.__string is set to input parameter
+		self.__setterUsed = False # Internal variable self.__setterUsed checks if setter was used to set self.__string
 
+	# Returns self.__string
 	def getString(self):
-		return self._string
-
+		return # BUG!! Should return self.__string
+	
+	# Returns True if setString() has been called, False otherwise
+	def setterUsed(self):
+		return self.__setterUsed
+	
+	# Sets self.__string without manipulating it directly
 	def setString(self, string):
-		self._string = string
+		self.__setterUsed = True
+		self.__string = string
 
+	# Prints out self.__string
 	def printString(self):
-		print self._string
-
-	def printLetters(self):
-		for s in self._string:
-			print s
+		print self.__string
